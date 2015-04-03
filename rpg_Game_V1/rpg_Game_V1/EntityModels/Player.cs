babc1548bbba1660fe7e068ffb87895b0e60c52d
IvoPaunov <ivo.paunov@gmail.com>
2015-04-03 12:45:04 +0300

    RC

--- rpg_Game_V1/rpg_Game_V1/EntityModels/Player.cs
+++ rpg_Game_V1/rpg_Game_V1/EntityModels/Player.cs
@@ -9,15 +9,14 @@ namespace rpg_Game_V1.EntityModels
 {
     class Player:GoodEntity
     {
-<<<<<<< HEAD
+
         private const int WeaponsLimit = 2;
         private const int ArmorLimit = 1;
         private const int TrinketsLimit = 4; 
-=======
+
         List<Ability> Skills = new List<Ability>();
 
 
->>>>>>> 7b05fa1db6d2270d686105cd079d4037deb1694c
 
         public Player(string name,int health,int mana, int stamina,
             int dexterity, int intelligence, int strenght)
