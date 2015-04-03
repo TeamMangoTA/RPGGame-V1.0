7b05fa1db6d2270d686105cd079d4037deb1694c
PavelNPavlov <pavelnapavlov@gmail.com>
2015-04-03 12:23:23 +0300

    123

--- rpg_Game_V1/rpg_Game_V1/EntityModels/Player.cs
+++ rpg_Game_V1/rpg_Game_V1/EntityModels/Player.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using rpg_Game_V1.AbilityModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +9,10 @@ namespace rpg_Game_V1.EntityModels
 {
     class Player:GoodEntity
     {
-        
+        List<Ability> Skills = new List<Ability>();
+
+
+
         public Player(string name,int health,int mana, int stamina,
             int dexterity, int intelligence, int strenght)
             :base(name,health,mana,stamina,dexterity,intelligence,strenght)
