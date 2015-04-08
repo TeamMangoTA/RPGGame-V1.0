b625b6dde8caf79bc46e70f8a70c60948addd5df
IvoPaunov <ivo.paunov@gmail.com>
2015-04-08 11:26:43 +0300

    ..

--- rpg_Game_V1/rpg_Game_V1/LooseWindow.Designer.cs
+++ rpg_Game_V1/rpg_Game_V1/LooseWindow.Designer.cs
@@ -29,13 +29,13 @@
         private void InitializeComponent()
         {
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
-            this.buttonRestart = new System.Windows.Forms.Button();
             this.buttonExit = new System.Windows.Forms.Button();
+            this.buttonRestart = new System.Windows.Forms.Button();
             this.pictureBoxLooser = new System.Windows.Forms.PictureBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
             this.tableLayoutPanel1.SuspendLayout();
-            this.flowLayoutPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLooser)).BeginInit();
+            this.flowLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // tableLayoutPanel1
@@ -44,8 +44,8 @@
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel1.Controls.Add(this.buttonExit, 1, 1);
-            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 0, 0);
             this.tableLayoutPanel1.Controls.Add(this.buttonRestart, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 0, 0);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
@@ -54,27 +54,6 @@
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 13.74723F));
             this.tableLayoutPanel1.Size = new System.Drawing.Size(922, 451);
             this.tableLayoutPanel1.TabIndex = 0;
-            // 
-            // flowLayoutPanel1
-            // 
-            this.tableLayoutPanel1.SetColumnSpan(this.flowLayoutPanel1, 2);
-            this.flowLayoutPanel1.Controls.Add(this.pictureBoxLooser);
-            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 3);
-            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(916, 382);
-            this.flowLayoutPanel1.TabIndex = 0;
-            // 
-            // buttonRestart
-            // 
-            this.buttonRestart.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.buttonRestart.Location = new System.Drawing.Point(3, 391);
-            this.buttonRestart.Name = "buttonRestart";
-            this.buttonRestart.Size = new System.Drawing.Size(455, 57);
-            this.buttonRestart.TabIndex = 1;
-            this.buttonRestart.Text = "TRY AGAIN";
-            this.buttonRestart.UseVisualStyleBackColor = true;
-            this.buttonRestart.Click += new System.EventHandler(this.buttonRestart_Click);
             // 
             // buttonExit
             // 
@@ -87,14 +66,35 @@
             this.buttonExit.UseVisualStyleBackColor = true;
             this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
             // 
+            // buttonRestart
+            // 
+            this.buttonRestart.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonRestart.Location = new System.Drawing.Point(3, 391);
+            this.buttonRestart.Name = "buttonRestart";
+            this.buttonRestart.Size = new System.Drawing.Size(455, 57);
+            this.buttonRestart.TabIndex = 1;
+            this.buttonRestart.Text = "TRY AGAIN";
+            this.buttonRestart.UseVisualStyleBackColor = true;
+            this.buttonRestart.Click += new System.EventHandler(this.buttonRestart_Click);
+            // 
             // pictureBoxLooser
             // 
-            this.pictureBoxLooser.Dock = System.Windows.Forms.DockStyle.Fill;
             this.pictureBoxLooser.Location = new System.Drawing.Point(3, 3);
             this.pictureBoxLooser.Name = "pictureBoxLooser";
-            this.pictureBoxLooser.Size = new System.Drawing.Size(100, 0);
+            this.pictureBoxLooser.Size = new System.Drawing.Size(913, 379);
+            this.pictureBoxLooser.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBoxLooser.TabIndex = 0;
             this.pictureBoxLooser.TabStop = false;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.flowLayoutPanel1, 2);
+            this.flowLayoutPanel1.Controls.Add(this.pictureBoxLooser);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 3);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(916, 382);
+            this.flowLayoutPanel1.TabIndex = 0;
             // 
             // LooseWindow
             // 
@@ -105,8 +105,8 @@
             this.Name = "LooseWindow";
             this.Text = "LooseWindow";
             this.tableLayoutPanel1.ResumeLayout(false);
-            this.flowLayoutPanel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLooser)).EndInit();
+            this.flowLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -115,8 +115,8 @@
 
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.Button buttonRestart;
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
         private System.Windows.Forms.PictureBox pictureBoxLooser;
-        private System.Windows.Forms.Button buttonRestart;
     }
 }
